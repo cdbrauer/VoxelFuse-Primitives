ec2d35688e90492922116ce3528ae8b037c0bd44
Cole B <cole.brauer@outlook.com>
2019-09-10 20:42:26 -0700

    Additional generation functions
    - Cube
    - Cuboid
    - Sphere
    - Cylinder
    - Cone
    - Pyramid

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # VoxelFuse-Primitives
-Classes for generating primitives using the VoxelFuse library
+Classes for generating primitives using the [VoxelFuse](https://github.com/cdbrauer/VoxelFuse) library.
+
+![Example 1](../master/images/main.png?raw=true)
--- voxelfuse_primitives_examples/primitive_generation/primitive_generation.py
+++ voxelfuse_primitives_examples/primitive_generation/primitive_generation.py
@@ -1,26 +1,31 @@
 """
 Copyright 2018
 Dan Aukes, Cole Brauer
-
-Generate coupon for tensile testing
 """
 
 import PyQt5.QtGui as qg
 import sys
-from voxelfuse.voxel_model import VoxelModel
+from voxelfuse_primitives.solid import Solid
 from voxelfuse.mesh import Mesh
 from voxelfuse.plot import Plot
-from voxelfuse_primitives.solid import Solid
 
 if __name__=='__main__':
-    # Settings
-    model1 = Solid.cube(5)
+    app1 = qg.QApplication(sys.argv)
+
+    model1 = Solid.cube(11, (0, 0, 0), 1)
+    model2 = Solid.cuboid((11, 20, 6), (13, 0, 0), 2)
+    model3 = Solid.sphere(5, (31, 5, 5), 3)
+    model4 = Solid.cylinder(5, 15, (44, 5, 0), 4)
+    model5 = Solid.cone(1, 5, 15, (57, 5, 0), 5)
+    model6 = Solid.pyramid(0, 5, 15, (70, 5, 0), 6)
+
+    model_result = model1 | model2 | model3 | model4 | model5 | model6
 
     # Create mesh data
-    mesh1 = Mesh.fromVoxelModel(model1)
+    mesh1 = Mesh.fromVoxelModel(model_result)
 
     # Create plot
-    plot1 = Plot(mesh1, grids=True)
+    plot1 = Plot(mesh1)
     plot1.show()
     app1.processEvents()
 
